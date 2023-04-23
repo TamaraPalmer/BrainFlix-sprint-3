25b9fbaace2f94a1b09e1b91a803b18e96787deb
Tamara Palmer <palmert974@macomb.com>
2023-04-23 14:57:47 -0400

    completed server to connect front end

--- routes/videos.js
+++ routes/videos.js
@@ -40,26 +40,59 @@ router.get("/:id", (req, res) => {
 // POST a new video
 router.post("/", (req, res) => {
   console.log(req.body);
-  const newVideos = {
+  const newVideo = {
     id: uuidv4(),
     title: req.body.title,
+    image: "https://i.imgur.com/i6S8m7I.jpg",
     description: req.body.description,
   };
 
-  const videos = readVideosFile;
-  videos.push(newVideos);
-  fs.writeFileSync(".data/videos.json", JSON.stringify(videos));
+  const videos = readVideosFile();
+  videos.push(newVideo);
+  fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
 
-  res.status(201).json(newVideos);
+  const newVideoDetails = {
+    id: newVideo.id,
+    title: newVideo.title,
+    image: newVideo.image,
+    description: newVideo.description,
+    comments: [
+      {
+        id: "35bba08b-1b51-4153-ba7e-6da76b5ec1b9",
+        name: "Micheal Lyons",
+        comment:
+          "They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.",
+        likes: 0,
+        timestamp: 1628522461000,
+      },
+      {
+        id: "091de676-61af-4ee6-90de-3a7a53af7521",
+        name: "Gary Wong",
+        comment:
+          "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+        likes: 0,
+        timestamp: 1626359541000,
+      },
+      {
+        id: "66b7d3c7-4023-47f1-a02c-520c9ca187a6",
+        name: "Theodore Duncan",
+        comment:
+          "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Every time I see him I feel instantly happy! He’s definitely my favorite ever!",
+        likes: 0,
+        timestamp: 1626011132000,
+      },
+    ],
+  };
+
+  const videoDetails = readVideoDetailsFile();
+  videoDetails.push(newVideoDetails);
+  fs.writeFileSync("./data/video-details.json", JSON.stringify(videoDetails));
+
+  res.status(201).json(newVideo);
 });
 
-
-// const videos = readVideoDetailsFile;
-// videos.push(newVideos);
-// fs.writeFileSync(".data/video-detail.json", JSON.stringify(videos));
-
-// res.status(201).json(newVideos);
 // });
+
 //   videos.push(newVideo);
 //   writeVideosFile(videos);
 //   res.status(201).json(newVideo);
